4723f165e3315f9f4e7253ea92865f6dcd8722cc
Cédric de Saint Martin <cdesaintmartin@wiremind.fr>
2020-11-08 21:40:43 +0100

    Add optional restore.

--- restore_mac.sh
+++ restore_mac.sh
@@ -7,6 +7,8 @@ brew install gnu-sed --with-default-names
 brew cask
 brew cask install iterm2 istat-menus quicksilver nextcloud bettertouchtool spectacle clipy
 brew cask install caskroom/versions/firefox-developer-edition
+# Optional
+brew cask install amethyst cyberduck gimp istat-menus nextcloud vlc android-platform-tools docker github iterm2 osxfuse textmate webex-meetings bettertouchtool dolphin google-chrome karabiner the-unarchiver wireshark buttercup google-cloud-sdk kubernetic pycharm transmission xquartz clipy google-drive-file-stream macdown quicksilver veracrypt zoom console font-hack-nerd-font grandperspective microsoft-teams spectacle visual-studio-code
 
 #mas install 497799835 # Xcode
 
